7a59e6388c8b98864e0d80c6c9fede0fd964225b
Sebastian Ludwig <sebastian@lurado.de>
2015-10-04 14:50:27 +0200

    Sped up square drawing by using a cached CALayer insteadof recreating everything all the time.

--- Shelftris/Square.m
+++ Shelftris/Square.m
@@ -51,85 +51,104 @@
 	[Square drawSqureInRect:squareRect withBaseColor:self.baseColor inContext:context];
 }
 
++ (CALayer *)squareWithSize:(CGSize)size baseColor:(UIColor *)baseColor
+{
+    static CALayer *cachedLayer = nil;
+    static UIColor *cachedColor = nil;
+    static CGSize cachedSize;
+    
+    if ([baseColor isEqual:cachedColor] && CGSizeEqualToSize(size, cachedSize)) {
+        return cachedLayer;
+    }
+    
+    CGFloat baseHue, baseSaturation, baseBrightness, baseAlpha;
+    if (![baseColor getHue:&baseHue saturation:&baseSaturation brightness:&baseBrightness alpha:&baseAlpha]) {
+        return nil;
+    }
+    
+    CGRect frame = CGRectMake(0, 0, size.width, size.height);
+    
+    CGFloat inset = size.width / 8.0;
+    CGRect centerRect = CGRectInset(frame, inset, inset);
+    centerRect = CGRectIntegral(centerRect);
+    
+    CALayer *layer = [CALayer new];
+    layer.frame = frame;
+    
+    // center
+    layer.backgroundColor = baseColor.CGColor;
+
+    // sides
+    {
+        UIColor *sideColor = [UIColor colorWithHue:baseHue saturation:baseSaturation brightness:(baseBrightness - 0.1) alpha:baseAlpha];
+        CGMutablePathRef path = CGPathCreateMutable();
+        // left
+        CGPathMoveToPoint(path, NULL, frame.origin.x, frame.origin.y);
+        CGPathAddLineToPoint(path, NULL, frame.origin.x, CGRectGetMaxY(frame));
+        CGPathAddLineToPoint(path, NULL, centerRect.origin.x, CGRectGetMaxY(centerRect));
+        CGPathAddLineToPoint(path, NULL, centerRect.origin.x, centerRect.origin.y);
+        
+        // right
+        CGPathMoveToPoint(path, NULL, CGRectGetMaxX(frame), frame.origin.y);
+        CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(frame), CGRectGetMaxY(frame));
+        CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(centerRect), CGRectGetMaxY(centerRect));
+        CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(centerRect), centerRect.origin.y);
+        
+        CAShapeLayer *sides = [CAShapeLayer new];
+        sides.path = path;
+        sides.fillColor = sideColor.CGColor;
+        [layer addSublayer:sides];
+        CGPathRelease(path);
+    }
+    
+    // top
+    {
+        UIColor *topColor = [UIColor colorWithHue:baseHue saturation:(baseSaturation - 0.4) brightness:baseBrightness alpha:baseAlpha];
+
+        CGMutablePathRef path = CGPathCreateMutable();
+        CGPathMoveToPoint(path, NULL, frame.origin.x, frame.origin.y);
+        CGPathAddLineToPoint(path, NULL, centerRect.origin.x, centerRect.origin.y);
+        CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(centerRect), centerRect.origin.y);
+        CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(frame), frame.origin.y);
+        
+        CAShapeLayer *top = [CAShapeLayer new];
+        top.path = path;
+        top.fillColor = topColor.CGColor;
+        [layer addSublayer:top];
+        CGPathRelease(path);
+    }
+    
+    // bottom
+    {
+        UIColor *bottomColor = [UIColor colorWithHue:baseHue saturation:baseSaturation brightness:(baseBrightness - 0.4) alpha:baseAlpha];
+        
+        CGMutablePathRef path = CGPathCreateMutable();
+        CGPathMoveToPoint(path, NULL, frame.origin.x, CGRectGetMaxY(frame));
+        CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(frame), CGRectGetMaxY(frame));
+        CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(centerRect), CGRectGetMaxY(centerRect));
+        CGPathAddLineToPoint(path, NULL, centerRect.origin.x, CGRectGetMaxY(centerRect));
+        
+        CAShapeLayer *bottom = [CAShapeLayer new];
+        bottom.path = path;
+        bottom.fillColor = bottomColor.CGColor;
+        [layer addSublayer:bottom];
+        CGPathRelease(path);
+    }
+    
+    cachedLayer = layer;
+    cachedColor = baseColor;
+    cachedSize = size;
+    
+    return layer;
+}
+
 + (void)drawSqureInRect:(CGRect)rect withBaseColor:(UIColor *)baseColor inContext:(CGContextRef)context
 {
-	CGFloat baseHue, baseSaturation, baseBrightness, baseAlpha;
-	if (![baseColor getHue:&baseHue saturation:&baseSaturation brightness:&baseBrightness alpha:&baseAlpha]) {
-		return;
-	}
-	
-	rect = CGRectIntegral(rect);
-	
-	CGFloat inset = rect.size.width / 8.0;
-	CGRect centerRect = CGRectInset(rect, inset, inset);
-	centerRect = CGRectIntegral(centerRect);
-		
-	CGContextFillRect(context, rect);
-	
-	CGContextSaveGState(context);
-	{
-		CGMutablePathRef path = CGPathCreateMutable();
-		CGPathMoveToPoint(path, NULL, rect.origin.x, rect.origin.y);
-		CGPathAddLineToPoint(path, NULL, rect.origin.x, CGRectGetMaxY(rect));
-		CGPathAddLineToPoint(path, NULL, centerRect.origin.x, CGRectGetMaxY(centerRect));
-		CGPathAddLineToPoint(path, NULL, centerRect.origin.x, centerRect.origin.y);
-		
-		UIColor *sideColor = [UIColor colorWithHue:baseHue saturation:baseSaturation brightness:(baseBrightness - 0.1) alpha:baseAlpha];
-		CGContextSetFillColorWithColor(context, [sideColor CGColor]);
-		CGContextAddPath(context, path);
-		CGContextFillPath(context);
-		CGPathRelease(path);
-	}
-	CGContextRestoreGState(context);
-	
-	CGContextSaveGState(context);
-	{
-		CGMutablePathRef path = CGPathCreateMutable();
-		CGPathMoveToPoint(path, NULL, CGRectGetMaxX(rect), rect.origin.y);
-		CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(rect), CGRectGetMaxY(rect));
-		CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(centerRect), CGRectGetMaxY(centerRect));
-		CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(centerRect), centerRect.origin.y);
-		
-		UIColor *sideColor = [UIColor colorWithHue:baseHue saturation:baseSaturation brightness:(baseBrightness - 0.1) alpha:baseAlpha];
-		CGContextSetFillColorWithColor(context, [sideColor CGColor]);
-		CGContextAddPath(context, path);
-		CGContextFillPath(context);
-		CGPathRelease(path);
-	}
-	CGContextRestoreGState(context);
-		
-	CGContextSaveGState(context);
-	{
-		CGMutablePathRef path = CGPathCreateMutable();
-		CGPathMoveToPoint(path, NULL, rect.origin.x, rect.origin.y);
-		CGPathAddLineToPoint(path, NULL, centerRect.origin.x, centerRect.origin.y);
-		CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(centerRect), centerRect.origin.y);
-		CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(rect), rect.origin.y);
-		CGContextAddPath(context, path);
-		UIColor *topColor = [UIColor colorWithHue:baseHue saturation:(baseSaturation - 0.4) brightness:baseBrightness alpha:baseAlpha];
-		CGContextSetFillColorWithColor(context, [topColor CGColor]);
-		CGContextFillPath(context);
-		CGPathRelease(path);
-	}
-	CGContextRestoreGState(context);
-	
-	CGContextSaveGState(context);
-	{
-		CGMutablePathRef path = CGPathCreateMutable();
-		CGPathMoveToPoint(path, NULL, rect.origin.x, CGRectGetMaxY(rect));
-		CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(rect), CGRectGetMaxY(rect));
-		CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(centerRect), CGRectGetMaxY(centerRect));
-		CGPathAddLineToPoint(path, NULL, centerRect.origin.x, CGRectGetMaxY(centerRect));
-		CGContextAddPath(context, path);
-		UIColor *bottomColor = [UIColor colorWithHue:baseHue saturation:baseSaturation brightness:(baseBrightness - 0.4) alpha:baseAlpha];
-		CGContextSetFillColorWithColor(context, [bottomColor CGColor]);
-		CGContextFillPath(context);
-		CGPathRelease(path);
-	}
-	CGContextRestoreGState(context);
-
-	CGContextSetFillColorWithColor(context, [baseColor CGColor]);
-	CGContextFillRect(context, centerRect);
+    CALayer *square = [self squareWithSize:rect.size baseColor:baseColor];
+    
+    CGContextTranslateCTM(context, rect.origin.x, rect.origin.y);
+    [square renderInContext:context];
+    CGContextTranslateCTM(context, -rect.origin.x, -rect.origin.y);
 }
 
 @end
